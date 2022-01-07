fac9c468a64b5f111f89baf47fd515c2c5107d09
Abdullah Burak Aksoy <aburakaksoy@hotmail.com>
2022-01-07 20:30:01 +0300

    Second Commit

--- src/test/java/com/cybertek/tests/ChercherTest.java
+++ src/test/java/com/cybertek/tests/ChercherTest.java
@@ -1,7 +1,12 @@
 package com.cybertek.tests;
 
 import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
@@ -12,6 +17,7 @@ import java.util.concurrent.TimeUnit;
 public class ChercherTest {
 
     WebDriver driver;
+    WebDriverWait wait;
 
     @BeforeMethod
     public void setUp(){
@@ -29,6 +35,14 @@ public class ChercherTest {
 
     @Test
     public void alertPresentTest(){
+        WebElement initiateAlert = driver.findElement(By.id("alert"));
+        initiateAlert.click();
+
+        wait =  new WebDriverWait(driver,10);
+        wait.until(ExpectedConditions.alertIsPresent());
+
+        Alert alert = driver.switchTo().alert();
+        alert.accept();
 
     }
 
